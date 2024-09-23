80877412a828c96da5f0cfd204a8c84dcbbf8a80
Didier-Ro <ortizrojeroh@gmail.com>
2024-09-22 18:16:21 -0600

    Rebind

--- Assets/Editor/PlayerInputEditor.cs
+++ Assets/Editor/PlayerInputEditor.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static GameInput;
 
 [CustomEditor(typeof(GameInput))]
 public class PlayerInputEditor : Editor
@@ -29,9 +31,28 @@ public class PlayerInputEditor : Editor
 
                 if (playerInput != null)
                 {
-                    ShowBindings("Jump", playerInput.Player.Jump);
+                    /*ShowBindings("Jump", playerInput.Player.Jump);
                     ShowBindings("Shoot", playerInput.Player.Shoot);
-                    ShowBindings("Movement", playerInput.Player.Movement);
+                    ShowBindings("Movement", playerInput.Player.Movement);*/
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Box("Jump Keyboard: ");
+                    GUILayout.Box(gameInput.GetBindingText(GameInput.Binding.Jump));
+                    EditorGUILayout.EndHorizontal();
+                    GUILayout.Space(10);
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Box("Jump Gamepad: ");
+                    GUILayout.Box(gameInput.GetBindingText(GameInput.Binding.Gamepad_Jump));
+                    EditorGUILayout.EndHorizontal();
+                    GUILayout.Space(10);
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Box("Shoot Keyboard: ");
+                    GUILayout.Box(gameInput.GetBindingText(GameInput.Binding.Shoot));
+                    EditorGUILayout.EndHorizontal();
+                    GUILayout.Space(10);
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Box("Shoot Gamepad: ");
+                    GUILayout.Box(gameInput.GetBindingText(GameInput.Binding.Gamepad_Shoot));
+                    EditorGUILayout.EndHorizontal();
                 }
                 else
                 {
@@ -43,18 +64,56 @@ public class PlayerInputEditor : Editor
 
         if (Application.isPlaying) 
         {
+            GUILayout.Space(10);
+            GUILayout.Box("Movement Vector");
+
             Vector2 movementVector = gameInput.GetMovementVectorNormalize();
             GUILayout.Label("Movement Vector: " + movementVector);
+
+            GUILayout.Space(10);
+            GUILayout.Box("Simulate Actions");
 
             if (GUILayout.Button("Jump"))
             {
                 gameInput.SimulateJump();
             }
 
+            GUILayout.Space(10);
+
             if (GUILayout.Button("Shoot"))
             {
                 gameInput.SimulateShoot();
             }
+
+            GUILayout.Space(25);
+            GUILayout.Box("Rebind Zone");
+
+            if (GUILayout.Button("Rebind Jump Keyboard"))
+            {
+                gameInput.RebindBinding(GameInput.Binding.Jump, () => { });
+            }
+
+            GUILayout.Space(10);
+
+            if (GUILayout.Button("Rebind Jump Gamepad"))
+            {
+                gameInput.RebindBinding(GameInput.Binding.Gamepad_Jump, () => { });
+            }
+
+            GUILayout.Space(10);
+
+            if (GUILayout.Button("Rebind Shoot Keyboard"))
+            {
+                gameInput.RebindBinding(GameInput.Binding.Shoot, () => { });
+            }
+
+            GUILayout.Space(10);
+
+            if (GUILayout.Button("Rebind Shoot Gamepad"))
+            {
+                gameInput.RebindBinding(GameInput.Binding.Gamepad_Shoot, () => { });
+            }
+
         }
         else
         {
--- Assets/[Scripts]/GameInput.cs
+++ Assets/[Scripts]/GameInput.cs
@@ -4,19 +4,39 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+    private const string PLAYER_PREFS_BINDINGS = "InputBindings";
+
     public static GameInput Instance { get; private set; }
 
     public event EventHandler OnJumpAction;
     public event EventHandler OnShootAction;
 
+    public event EventHandler OnBindingRebind;
+
     private PlayerInput playerInput;
 
+    public enum Binding
+    {
+        Move_Up,
+        Move_Down,
+        Move_Left,
+        Move_Right,
+        Jump,
+        Shoot,
+        Gamepad_Jump,
+        Gamepad_Shoot,
+    }
 
     private void Awake()
     {
         Instance = this;
 
         playerInput = new PlayerInput();
+
+        if (PlayerPrefs.HasKey(PLAYER_PREFS_BINDINGS))
+        {
+            playerInput.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_BINDINGS));
+        }
 
         playerInput.Enable();
 
@@ -30,6 +50,11 @@ public class GameInput : MonoBehaviour
         if (!Application.isPlaying && playerInput == null)
         {
             playerInput = new PlayerInput();
+
+            if (PlayerPrefs.HasKey(PLAYER_PREFS_BINDINGS))
+            {
+                playerInput.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_BINDINGS));
+            }
         }
     }
 
@@ -50,6 +75,86 @@ public class GameInput : MonoBehaviour
         inputVector = inputVector.normalized;
 
         return inputVector;
+    }
+
+    public string GetBindingText(Binding binding)
+    {
+        switch (binding)
+        {
+            default:
+            case Binding.Move_Up:
+                return playerInput.Player.Movement.bindings[1].ToDisplayString();
+            case Binding.Move_Down:
+                return playerInput.Player.Movement.bindings[2].ToDisplayString();
+            case Binding.Move_Left:
+                return playerInput.Player.Movement.bindings[3].ToDisplayString();
+            case Binding.Move_Right:
+                return playerInput.Player.Movement.bindings[4].ToDisplayString();
+            case Binding.Jump:
+                return playerInput.Player.Jump.bindings[0].ToDisplayString();
+            case Binding.Shoot:
+                return playerInput.Player.Shoot.bindings[0].ToDisplayString();
+            case Binding.Gamepad_Jump:
+                return playerInput.Player.Jump.bindings[1].ToDisplayString();
+            case Binding.Gamepad_Shoot:
+                return playerInput.Player.Shoot.bindings[1].ToDisplayString();
+        }
+    }
+
+    public void RebindBinding(Binding binding, Action onActionRebound)
+    {
+        playerInput.Player.Disable();
+
+        InputAction inputAction;
+        int bindingIndex;
+        switch (binding)
+        {
+            default:
+            case Binding.Move_Up:
+                inputAction = playerInput.Player.Movement;
+                bindingIndex = 1;
+                break;
+            case Binding.Move_Down:
+                inputAction = playerInput.Player.Movement;
+                bindingIndex = 2;
+                break;
+            case Binding.Move_Left:
+                inputAction = playerInput.Player.Movement;
+                bindingIndex = 3;
+                break;
+            case Binding.Move_Right:
+                inputAction = playerInput.Player.Movement;
+                bindingIndex = 4;
+                break;
+            case Binding.Jump:
+                inputAction = playerInput.Player.Jump;
+                bindingIndex = 0;
+                break;
+            case Binding.Shoot:
+                inputAction = playerInput.Player.Shoot;
+                bindingIndex = 0;
+                break;
+            case Binding.Gamepad_Jump:
+                inputAction = playerInput.Player.Jump;
+                bindingIndex = 1;
+                break;
+            case Binding.Gamepad_Shoot:
+                inputAction = playerInput.Player.Shoot;
+                bindingIndex = 1;
+                break;
+        }
+        inputAction.PerformInteractiveRebinding(bindingIndex)
+            .OnComplete(callback => {
+                callback.Dispose();
+                playerInput.Player.Enable();
+                onActionRebound();
+
+                PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS, playerInput.SaveBindingOverridesAsJson());
+                PlayerPrefs.Save();
+
+                OnBindingRebind?.Invoke(this, EventArgs.Empty);
+            })
+            .Start();
     }
 
     public void SimulateJump()
